fe5e98a57b2951dbdfd6114b502d8c4202e67bad
oliewalcp <811299838@qq.com>
2017-05-25 21:49:59 +0800

    解决了AI1作为玩家2时清空文件内容的问题

--- buweiqi/ChessBoard_Header/AI.h
+++ buweiqi/ChessBoard_Header/AI.h
@@ -1,10 +1,15 @@
 #pragma once
 #ifndef AI_H_INCLUDED
 #define AI_H_INCLUDED
+#include "SIP.h"
+#include "../FileSystem_Header/FileSystem.h"
 
 class AI
 {
 public:
+	bool link;//标记是否已打开文件
+	FileSystem FS; //文件操作对象
+	QUADRANT Qua; //记录走棋纪录
 	virtual void GetPosition(int &line, int &column, int onTurn) = 0;
 };
 
